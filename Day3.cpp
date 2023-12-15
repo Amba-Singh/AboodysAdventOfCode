a22870649371a7a39709633c3265e69a1c14fb2d
Zyxify0 <152653600+Zyxify0@users.noreply.github.com>
2023-12-15 19:54:20 +0200

    Update Day3.cpp

--- Day3.cpp
+++ Day3.cpp
@@ -1,63 +1,147 @@
+				//          	PART 1
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
-int countInstances(std::string s, char x) {int count = 0;for (int i = 0; i < s.length(); i++) {if (s[i] == x) {count++;}}return count;}
+std::vector<std::vector<char>>  theList;
+std::vector<int> indexesToBeChecked;
 
-std::vector <std::string> splitstrings;
-void ABDOSsplit(std::string x, char delim) { std::vector<int> delimPlaces; for (int i = 0; i < x.length(); i++) { if (x[i] == delim) { delimPlaces.push_back(i); } }for (int n = 0; n < delimPlaces.size(); n++) { splitstrings.push_back(x.substr(delimPlaces[n] + 1, (delimPlaces[n + 1]) - delimPlaces[n])); } }
-
-std::vector<int> listIndexesToBeChecked(std::vector<int> indexesOfNumbers){
-    std::vector<int> indexesToBeChecked;
-    for(int i = 0 ; i < indexesOfNumbers.size() ; i++){
-        indexesToBeChecked.push_back((indexesOfNumbers[i]+141));
-        indexesToBeChecked.push_back((indexesOfNumbers[i]-141));
-        indexesToBeChecked.push_back(indexesOfNumbers.front()-1);
-        indexesToBeChecked.push_back((indexesOfNumbers.back()+1));
-
-        indexesToBeChecked.push_back((indexesOfNumbers[i]+142));
-        indexesToBeChecked.push_back((indexesOfNumbers[i]+140));
-        indexesToBeChecked.push_back((indexesOfNumbers[i]-142));
-        indexesToBeChecked.push_back((indexesOfNumbers[i]-140));
-    }
-
-    return indexesToBeChecked;
+std::vector<int> returnindexesToBeChecked(std::vector<int> x) {
+    std::vector<int> tempindexes;
+    if (!(x.front() == 0)) { tempindexes.push_back(x.front() - 1); tempindexes.push_back(x.front());}else{ tempindexes.push_back(x.front()); }
+    if (!(x.back() == 139)) { tempindexes.push_back(x.back() + 1); tempindexes.push_back(x.back());}else{ tempindexes.push_back(x.back()); }
+    if (x.size() == 3) {tempindexes.push_back(x[1]);}
+    return tempindexes;
 }
 
-int main() {
 
-    //getting the text input
-	std::fstream myFile;
-	myFile.open("day3.txt",std::fstream::in);
-	std::string oneLine;
-    std::vector<std::vector<char>> theList;
-	//iterate through every line
-	while(!myFile.eof()){
-        getline(myFile,oneLine);
+int main()
+{
+    std::fstream ff;
+    ff.open("day3.txt", std::fstream::in);
 
-        //iterate through every character and add it to a temporary vector
-        std::vector<char> temporaryVector;
-        for(int i=0;i<oneLine.length();i++){temporaryVector.push_back(oneLine[i]);}
-        theList.push_back(temporaryVector);
-	}
+    std::string oneLine;
+    for (int a = 0;!ff.eof(); a++) {
+        getline(ff, oneLine);
+        std::vector<char> tempvector;
+        for (int b = 0; b < oneLine.length(); b++) {
+            tempvector.push_back(oneLine[b]);
+        }
+        theList.push_back(tempvector);
+    }
+    int total = 0;
 
-	//iterate through every character
-    for(int a=0;a<theList.size();a++){
-        for(int b=0;b<theList[a].size();b++){std::cout << theList[a][b];
-        //std::cout << std::endl;
-        int charAsInt = theList[a][b] - '0';
-        int charAfterAsInt = theList[a][b+1] - '0';
-        int charAfterAfterAsInt = theList[a][b+2] - '0';
-        int number;
-        if(isdigit(charAsInt)){
-            if(isdigit(charAfterAsInt)){
-                if(isdigit(charAfterAfterAsInt)){
-                    number = charAsInt*100 + charAfterAsInt*10 + charAfterAfterAsInt;}
-                else{number = charAsInt*10 + charAfterAsInt;}
-            }else{number = charAsInt;}
+    for (int a = 0; a < theList.size(); a++) {
+        for (int b = 0; b < theList[a].size(); b++) {
+            bool cond = false;
+            int threeDigitNumber;
+            std::vector<int> indexes;
+            //std::cout << theList[a][b];
+            if (isdigit(theList[a][b]) && cond == false && b != 139 && b != 138) {
+                if (isdigit(theList[a][b+1]) && cond == false && b != 139) {
+                    if (isdigit(theList[a][b + 2]) && cond == false) {
+                        threeDigitNumber = (theList[a][b] - '0') * 100 + (theList[a][b+1] - '0') * 10 + (theList[a][b+2] - '0');
+                        std::cout << threeDigitNumber << std::endl;
+                        theList[a][b] = '.';
+                        theList[a][b + 1] = '.';
+                        theList[a][b + 2] = '.';
+                        indexes.push_back(b);
+                        indexes.push_back(b+1);
+                        indexes.push_back(b+2);
+                        std::vector<int> indexesToBeChecked = returnindexesToBeChecked(indexes);
+                        for (int index = 0; index < indexesToBeChecked.size(); index++) {
+                            if (a != 0 && a < (theList.size()-1)) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.' || theList[a-1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else if (a == 0) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a - 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        threeDigitNumber = (theList[a][b] - '0') * 10 + (theList[a][b + 1] - '0');
+                        std::cout << threeDigitNumber << std::endl;
+                        theList[a][b] = '.';
+                        theList[a][b + 1] = '.';
+                        indexes.push_back(b);
+                        indexes.push_back(b+1);
+                        std::vector<int> indexesToBeChecked = returnindexesToBeChecked(indexes);
+                        for (int index = 0; index < indexesToBeChecked.size(); index++) {
+                            if (a != 0 && a < (theList.size() - 1)) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.' || theList[a - 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else if (a == 0) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a - 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    threeDigitNumber = theList[a][b] - '0';
+                    std::cout << threeDigitNumber << std::endl;
+                    theList[a][b] = '.';
+                    indexes.push_back(b);
+                    std::vector<int> indexesToBeChecked = returnindexesToBeChecked(indexes);
+                                            for (int index = 0; index < indexesToBeChecked.size(); index++) {
+                            if (a != 0 && a < (theList.size()-1)) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.' || theList[a-1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else if (a == 0) {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a + 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                            else {
+                                if (theList[a][indexesToBeChecked[index]] != '.' || theList[a - 1][indexesToBeChecked[index]] != '.') {
+                                    std::cout << "Hello\n";
+                                    total += threeDigitNumber;
+                                    break;
+                                }
+                            }
+                        }
+                }
             }
         }
+        std::cout << "\n";
     }
-
-	return 0;
+    std::cout << "total is " << total;
 }
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+
